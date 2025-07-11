ab3458d62fa2c62e1861af68c52eef0e4f6479dd
jhony800 <kzhxjzhbz@gmail.com>
2025-07-11 16:14:37 -0300

    Create config.lua

---
+++ server/config.lua
@@ -0,0 +1,60 @@
+-- Eternal 7.4 - Configuração principal do servidor
+
+accountManager = false
+namelockManager = true
+newPlayerChooseVoc = true
+newPlayerSpawnPosX = 95
+newPlayerSpawnPosY = 117
+newPlayerSpawnPosZ = 7
+newPlayerTownId = 1
+newPlayerLevel = 8
+newPlayerMagicLevel = 0
+generateAccountNumber = false
+
+redSkullLength = 7 * 24 * 60 * 60
+blackSkullLength = 14 * 24 * 60 * 60
+dailyFragsToRedSkull = 3
+weeklyFragsToRedSkull = 5
+monthlyFragsToRedSkull = 10
+dailyFragsToBlackSkull = 6
+weeklyFragsToBlackSkull = 10
+monthlyFragsToBlackSkull = 20
+dailyFragsToBanishment = 7
+weeklyFragsToBanishment = 15
+monthlyFragsToBanishment = 30
+
+useBlackSkull = true
+advancedFragList = false
+
+-- Exp/Skill
+rateExp = 1
+rateSkill = 1
+rateLoot = 1
+rateMagic = 1
+rateSpawn = 1
+
+-- Connection
+ip = "127.0.0.1"
+bindOnlyConfiguredIpAddress = false
+loginPort = 7171
+gamePort = 7172
+statusPort = 7171
+maxPlayers = 100
+motd = "Bem-vindo ao Eternal 7.4 - Um verdadeiro Old School!"
+onePlayerOnlinePerAccount = true
+
+-- Database
+sqlType = "mysql"
+sqlHost = "localhost"
+sqlPort = 3306
+sqlUser = "root"
+sqlPass = "473488"
+sqlDatabase = "eternal"
+sqlKeepAlive = 0
+
+-- Account do GOD já criada:
+-- Account: 473488
+-- Password: jhx2025
+
+-- Premium
+freePremium = true
